1ae456c27fc550c9389b3f951aa6ded2f0fe7ce3
holmesaky <119851409+holmesaky@users.noreply.github.com>
2023-02-19 21:02:13 +0300

    Add files via upload

--- project/script.js
+++ project/script.js
@@ -63,31 +63,44 @@ function show() {
     }
 }
 
-function show1() {
-    const searchBox = document.querySelector(".Add-box");
-    const searchBtn = document.querySelector(".Add-icon");
-    const searchInput = document.querySelector("add-course");
-
-
-    if (!searchBtn.classList.contains("active")) {
-        searchBox.classList.add("active");
-        searchBtn.classList.add("active");
-        searchInput.classList.add("active");
-        cancelBtn.classList.add("active");
-        searchInput.focus();
-        if (searchInput.value != "") {
-            var values = searchInput.value;
-            searchData.classList.remove("active");
-            searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
-        } else {
-            searchData.textContent = "";
+function show1(temp) {
+    const searchBox = document.querySelector(temp);
+    const cars = [".down-5", ".down-4"];
+    for (var i = 0; i < cars.length; i++) {
+        if (temp !== cars[i]) {
+            document.querySelector(cars[i]).classList.add("fa-caret-down");
         }
+    }
+
+    if (searchBox.classList.contains("fa-caret-down")) {
+
+        searchBox.classList.add("fa-caret-up");
+        searchBox.classList.remove("fa-caret-down");
+
     } else {
-        searchBox.classList.remove("active");
-        searchBtn.classList.remove("active");
-        searchInput.classList.remove("active");
-        cancelBtn.classList.remove("active");
-        searchData.classList.toggle("active");
-        searchInput.value = "";
+        searchBox.classList.add("fa-caret-down");
+        searchBox.classList.remove("fa-caret-up");
+    }
+}
+
+function show2(temp) {
+    const searchBox = document.querySelector(temp);
+    const cars = [".down-1", ".down-2", ".down-3"];
+    for (var i = 0; i < cars.length; i++) {
+        if (temp !== cars[i]) {
+            document.querySelector(cars[i]).classList.add("fa-caret-down");
+        }
+    }
+
+
+
+    if (searchBox.classList.contains("fa-caret-down")) {
+
+        searchBox.classList.add("fa-caret-up");
+        searchBox.classList.remove("fa-caret-down");
+
+    } else {
+        searchBox.classList.add("fa-caret-down");
+        searchBox.classList.remove("fa-caret-up");
     }
 }
